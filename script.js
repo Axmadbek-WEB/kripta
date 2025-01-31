d4e0ad55894dec25a338df40eaff595ae99a870c
Axmadbek-WEB <uzbytbs@gmail.com>
2025-02-01 01:47:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// const body=document.getElementsByTagName("body")[0]
+//     onclik = bitkoin=confirm(`bitkoin 1000 doldir turadi sotib olasmi?`)
+     
+//     if(bitkoin==true){
+//         bitkoinn=0
+//     }else if(bitkoin==false){
+//         bitkoinn=1000
+//     }
+
+
+
+// body.appendChild(bitkoinn)
